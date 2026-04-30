1f1cac8a036ac1da462a6298f04b3c2b51d23244
Carlosfelipepolaniasanabria <carlosf.polanias@unac.edu.co>
2026-04-29 19:11:44 -0500

    Añado función Perfilpublico junto a Info y perfil que son clases privadas que se utilizan en perfil y perfil es un props colocado en app.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
-import { ButtonProps, PropChildren } from "./components/Props";
+import { ButtonProps, PropChildren, perfil} from "./components/Props";
+
+
+class Usuario {
+  constructor(nombre, edad, avatar) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.avatar = avatar;
+  }
+}
 
 function App() {
+  const usuario = new Usuario("Juan", 30, "https://example.com/avatar.jpg");
+
   return (
     <>
       <ButtonProps text="Guardar" color="blue" />
@@ -14,6 +25,11 @@ function App() {
         </p>
         <ButtonProps text="Prop Button" color="green" /> {/*En este momento ButtonsProps es el hijo de ButtonChildren.*/}
       </PropChildren>
+
+      <perfil usuario={usuario} /> {/* Esto usuario viene de la clase Usuario y en efecto lo que esta en la clase se pasa a la función exportada de perfil en Props.jsx como
+      puede ver arriba hay una parte que llame a la función perfil y si entras veras a perfil que se compone de otras dos funciones son privadas porque no estan siendo exportadas
+      osea que se utilizan en la misma Props.jsx mientras que las que se exportan son funciones publicas que se pueden utilizar en otros jsx como por ejemplo perfil que lo estamos
+      utilizando en App.jsx */}
     </>
   );
 }
--- src/components/Props.jsx
+++ src/components/Props.jsx
@@ -24,3 +24,29 @@ export function PropChildren({
         </div>
     );
 }
+
+
+function Avatar({ url, nombre }) {
+    return (
+        <img src={url} alt={nombre} 
+    className="avatar" />
+    );}
+
+
+function Info({ nombre, edad }) {
+    return (
+        <div>
+            <h4>Nombre: {nombre}</h4>
+            <p>Edad: {edad}</p>
+        </div>
+    );
+}
+
+export function perfil({ usuario }) {
+    return (
+        <div className="perfil">
+            <Avatar url={usuario.avatar} nombre={usuario.nombre} />
+            <Info nombre={usuario.nombre} edad={usuario.edad} />
+        </div>
+    );
+}
