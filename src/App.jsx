fa8d6c441b7f9f2991041dae5e65bb26dce82e04
Carlosfelipepolaniasanabria <carlosf.polanias@unac.edu.co>
2026-05-20 18:54:46 -0500

    Clase 20052026 Use-Ref and Use-context and other use you will go to see in the class.

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,64 @@
 
-import React from "react";
-import { Contador, Formulario, Formulario_Objeto } from "./components/hook.jsx"; /*Importamos el componente contador que se encuentra en use_State.jsx*/
-import { Timer, CounterwithVariable, ListaPost } from "./components/Use_effect.jsx"; /*Importamos el componente Timer que se encuentra en Use_effect.jsx*/
-import { ButtonProps, PropChildren, Perfil } from "./components/Props";
+// import React from "react";
+// import { Contador, Formulario, Formulario_Objeto } from "./components/hook.jsx"; /*Importamos el componente contador que se encuentra en use_State.jsx*/
+// import { Timer, CounterwithVariable, ListaPost } from "./components/Use_effect.jsx"; /*Importamos el componente Timer que se encuentra en Use_effect.jsx*/
+// import { ButtonProps, PropChildren, Perfil } from "./components/Props";
+// import { Component1 } from "./components/Use_Context.jsx";
 
-class Usuario {
-  constructor(nombre, edad, avatar) {
-    this.nombre = nombre;
-    this.edad = edad;
-    this.avatar = avatar;
-  }
-}
+// class Usuario {
+//   constructor(nombre, edad, avatar) {
+//     this.nombre = nombre;
+//     this.edad = edad;
+//     this.avatar = avatar;
+//   }
+// }
+
+// function App() {
+//   const usuario = new Usuario(
+//     "Carlos Felipe Polania Sanabria",
+//     19,
+//     "https://i.pravatar.cc/150" // usa una imagen real
+//   );
+
+//   return (
+//     <>
+//       <ButtonProps text="Guardar" color="blue" />
+//       <ButtonProps text="Eliminar" color="red" />
+
+//       <PropChildren titulo="Contenido">
+//         <h1>Titulo</h1>
+//         <p>Este es el contenido dentro del componente.</p>
+//         <ButtonProps text="Prop Button" color="green" /> {/*En este momento ButtonsProps es el hijo de ButtonChildren.*/}
+//       </PropChildren>
+
+//       <Perfil usuario={usuario} />{/* Esto usuario viene de la clase Usuario y en efecto lo que esta en la clase se pasa a la función exportada de perfil en Props.jsx como
+//       puede ver arriba hay una parte que llame a la función perfil y si entras veras a perfil que se compone de otras dos funciones son privadas porque no estan siendo exportadas
+//       osea que se utilizan en la misma Props.jsx mientras que las que se exportan son funciones publicas que se pueden utilizar en otros jsx como por ejemplo perfil que lo estamos
+//       utilizando en App.jsx */}
+
+
+//       <Contador /> {/*Aqui estamos utilizando el componente contador que se encuentra en use_State.jsx*/} 
+//       <Formulario />
+//       <Formulario_Objeto />
+//       <Timer />
+//       <CounterwithVariable />
+//       <ListaPost /> 
+//     </>
+//   );
+// }
+
+// export default App;
+
+
+import React from "react";
+import { CounterwithRef } from "./components/Use-Ref.jsx";
+import { Component1 } from "./components/Use_Context.jsx";
 
 function App() {
-  const usuario = new Usuario(
-    "Carlos Felipe Polania Sanabria",
-    19,
-    "https://i.pravatar.cc/150" // usa una imagen real
-  );
-
   return (
     <>
-      <ButtonProps text="Guardar" color="blue" />
-      <ButtonProps text="Eliminar" color="red" />
-
-      <PropChildren titulo="Contenido">
-        <h1>Titulo</h1>
-        <p>Este es el contenido dentro del componente.</p>
-        <ButtonProps text="Prop Button" color="green" /> {/*En este momento ButtonsProps es el hijo de ButtonChildren.*/}
-      </PropChildren>
-
-      <Perfil usuario={usuario} />{/* Esto usuario viene de la clase Usuario y en efecto lo que esta en la clase se pasa a la función exportada de perfil en Props.jsx como
-      puede ver arriba hay una parte que llame a la función perfil y si entras veras a perfil que se compone de otras dos funciones son privadas porque no estan siendo exportadas
-      osea que se utilizan en la misma Props.jsx mientras que las que se exportan son funciones publicas que se pueden utilizar en otros jsx como por ejemplo perfil que lo estamos
-      utilizando en App.jsx */}
-
-
-      <Contador /> {/*Aqui estamos utilizando el componente contador que se encuentra en use_State.jsx*/} 
-      <Formulario />
-      <Formulario_Objeto />
-      <Timer />
-      <CounterwithVariable />
-      <ListaPost /> 
+      <CounterwithRef />
+      <Component1 />
     </>
   );
 }
---
+++ src/components/Use-Ref.jsx
@@ -0,0 +1,18 @@
+import { useRef } from "react";
+
+import { useState } from "react";
+
+export function CounterwithRef() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+    </div>
+  );
+}
